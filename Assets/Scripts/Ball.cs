4556fab3dd57d7d29a4cb48cfb7ace5dd675e14d
Anders Tolborg <anderstolborg@sybogames.com>
2016-09-02 16:54:34 +0200

    first commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	
+	public Paddle paddle;
+	private Vector3 paddleToBallVector;
+	private Rigidbody2D rigi;
+	private bool hasStarted = false;
+
+	void Awake() {
+		rigi = GetComponent<Rigidbody2D>();
+	}
+
+
+	void Start() {
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+
+
+	void Update() {
+		if (!hasStarted) {
+
+			// Lock the ball.
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+
+			// Handle launch of ball.
+			if (Input.GetMouseButtonDown(0)) {
+				print("Mouse clicked!");
+				hasStarted = true;
+				rigi.velocity = new Vector2(2f, 10f);
+			}
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+
+	public int maxHits;
+	public int timesHit;
+
+
+	void Start() {
+	}
+	
+
+	void Update() {
+	}
+		
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		timesHit++;
+	}
+
+
+}
